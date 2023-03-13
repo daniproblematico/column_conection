7367409ee1d370a7526c509f9784af4f15c825ee
daniproblematico <tabares.juan1403@gmail.com>
2023-03-13 14:48:37 -0500

    Now it works correctly with Etabs files.

--- para_el_jefe/columns_maker.py
+++ para_el_jefe/columns_maker.py
@@ -5,57 +5,49 @@ import numpy as np
 with open("midas.geom", "r") as f:
     building_data = json.load(f)
 
-#auxiliar functions
-
-
+# Auxiliar functions
 def beam_finder(element_id, levels):
-    # search what level the element is in
+    """Search what level the element is in
+
+    Args:
+        element_id (str): element id
+        levels (dict): dictionary with all the levels information processed
+
+    Returns:
+        list: list that contains the levels where the element is found
+    """
     encontrados = []
     for level in levels:
-        if element_id in levels[level]["key_beams"]:
-            encontrados.append(level)
-        elif element_id in levels[level]["key_nodes"]:
-            encontrados.append(level)
-        elif element_id in levels[level]["key_cols"]:
+        if (
+            element_id in levels[level]["key_beams"]
+            or element_id in levels[level]["key_nodes"]
+            or element_id in levels[level]["key_cols"]
+        ):
             encontrados.append(level)
 
     return encontrados
 
-def column_verify(item,columns,levels_info):
 
-    item_level=beam_finder(item, levels_info)
-    for i, banos in enumerate(columns):
-        if item in columns[i]:
-            return (item_level, i, columns[i])
-
-def element_finder(element, prodes_data, element_type):
-    """this function will find what actual element this element came from using prodes file infor
+def column_verify(item, columns, levels_info):
+    """Search what column the element is in, what level it is in, and the elements that are in the same column
 
     Args:
-        element (String): Name of the element
-        prodes_data (txt?): file with actual elements information
-        element_type (String): What kind of actual element i'm interested in ("V-" for Beams, "C-" for columns, "N-" for nervs)
+        item (str): element to be searched
+        columns (dict): dictionary with all the columns information processed
+        levels_info (dict): dictionary with all the levels information processed
 
-    Return: sorage: actual element id
+    Returns:
+        list: list that contains the levels where the element is found
+        str: column id
+        dict: dictionary with all the keys elements in the same column
     """
-
-    searcher = False
-    for i in prodes_data:
-        if searcher == True:
-            if element in i:
-                storage = beam
-                break
-            else:
-                searcher = False
-
-        if element_type in i:
-            searcher = True
-            beam = i
-    return storage
+    item_level = beam_finder(item, levels_info)
+    for i, banos in enumerate(columns):
+        if item in columns[i]:
+            return (item_level, i, columns[i].keys())
 
 
-
-#essential functions
+# Essential functions
 def create_node(id, global_info_nodes, story_heigth_acum, new_id):
     """Node information constructor
 
@@ -82,17 +74,17 @@ def create_node(id, global_info_nodes, story_heigth_acum, new_id):
 
 
 def calculate_lenght(element_nodei_coordinates, element_nodej_coordinates):
-    """calculates the lenght of an element on the x-y plane
+    """calculates the length of an element on the x-y plane
 
     Args:
         beam_nodei_coordinates (list): coordinates of the node i of the beam in the order [x,y]
         beam_nodej_coordinates (list): coordinates of the node j of the beam in the order [x,y]
 
     Returns:
-        beam_lenght: lenght of the beam
+        beam_lenght: length of the beam
     """
 
-    # calculates the lenght of the beam using pitagoras theorem
+    # Calculates the length of the beam using pitagoras theorem
     element_lenght = np.sqrt(
         (element_nodej_coordinates[1] - element_nodei_coordinates[1]) ** 2
         + (element_nodej_coordinates[0] - element_nodei_coordinates[0]) ** 2
@@ -106,7 +98,7 @@ def calculate_angle(
     beam_nodej_coordinates,
     column_node_coordinates,
 ):
-    """calculates the angle between the beam and the referency vector using the dot product
+    """calculates the angle between a beam element and the referency vector using dot product
 
     Args:
         beam_nodei_coordinates (list): coordinates of the node i of the beam in the order [x,y]
@@ -152,10 +144,10 @@ def extract_element(
     orientation,
     area,
     reinforcement,
-    lenght,
+    length,
 ):
 
-    """Extracts the information of the beams making them objects of the class Beam
+    """Creates a dictionary with the information of the beam element
 
     Args:
         levels (dict): contains all the informatios of the building by level
@@ -164,11 +156,10 @@ def extract_element(
         orientation (float): orientation of the beam in radians clockwise respect to the x axis
         area (list): area of the beam in the order [b,h]
         reinforcement (string): reinforcement of the beam
-        lenght (float): lenght of the beam
+        length (float): length of the beam
 
     Returns: dictionary with the information of the beam
     """
-
 
     return dict(
         element_id=element_id,
@@ -176,26 +167,28 @@ def extract_element(
         orientation=orientation,
         area=area,
         reinforcement=reinforcement,
-        lenght=lenght,
+        length=length,
         level_id=level,
     )
 
 
 def extract_levels(data, initial_heigth):
-    """Extracts the information of the levels making them objects of the class Level
+    """Extracts the information of the levels making them keys in the dictionary Levels
 
     Args:
         data (dict): contains all the informatios of the building from the json file
-        initial_heigth (float): initial heigth of the building
+        initial_heigth (float): reference height of the building
 
     Returns:
         levels: dictionary with the information organized by level
     """
+    #Dictionary that will contain all the information of the building
     levels = {}
+    #Node arbitrary identificator
     nodes_id = 0
     # For each level, the information will be storaged
     for story in reversed(data["pisos"]):
-        # define keys for the levels dict
+        # Define keys for the levels dict
         levels[story] = dict(
             id=story,
             key_nodes=[],
@@ -254,7 +247,7 @@ def extract_levels(data, initial_heigth):
     return levels
 
 
-def columns_assembler(elements):
+def columns_assembler(elements,tolerance):
     """find the vertical elements that are connected and assign them to a column with arbitrary name
 
     Args:
@@ -263,36 +256,65 @@ def columns_assembler(elements):
         columns: dictionary with the information organized by column
     """
 
-    #dictionary where the columns will be storaged and the namer of the columns
+    # dictionary where the columns will be storaged and the namer of the columns
     columns = {}
     namer = 0
-    
-    #for each element, the elements in the same coordinates will be storaged in a list
+
+    # for each element, the elements in the same coordinates will be storaged in a list
     for i in elements:
-        columns[namer]={}
+        columns[namer] = {}
         node_connector = elements[i]["node_t"]
         for j in elements:
             if (
-                abs(elements[j]["node_t"]["x_coord"] - node_connector["x_coord"]) < 0.001
-                and abs(elements[j]["node_t"]["y_coord"] - node_connector["y_coord"]) < 0.001
+                abs(elements[j]["node_t"]["x_coord"] - node_connector["x_coord"])
+                < tolerance
+                and abs(elements[j]["node_t"]["y_coord"] - node_connector["y_coord"])
+                < tolerance
             ):
-                columns[namer].update({j:elements[j]})
+                columns[namer].update({j: elements[j]})
         namer += 1
-    
-    #there will be repeated columns, this will be deleted to avoid errors
+
+    # there will be repeated columns, this will be deleted to avoid errors
     for i in columns:
-        repeated_elements=0
+        repeated_elements = 0
         for j in columns:
             if set(columns[i]) == set(columns[j]):
-                repeated_elements+=1
-            if repeated_elements>1:
+                repeated_elements += 1
+            if repeated_elements > 1:
                 columns[j].clear()
-        
+
     for empty in list(columns):
         if not columns[empty]:
             del columns[empty]
-    
+
     return columns
+
+
+#prodes functions
+def element_finder(element, prodes_data, element_type):
+    """this function will find what actual element this element came from using prodes file infor
+
+    Args:
+        element (String): Name of the element
+        prodes_data (txt?): file with actual elements information
+        element_type (String): What kind of actual element i'm interested in ("V-" for Beams, "C-" for columns, "N-" for nervs)
+
+    Return: sorage: actual element id
+    """
+
+    searcher = False
+    for i in prodes_data:
+        if searcher == True:
+            if element in i:
+                storage = beam
+                break
+            else:
+                searcher = False
+
+        if element_type in i:
+            searcher = True
+            beam = i
+    return storage
 
 
 levels = extract_levels(building_data, 0)
@@ -302,13 +324,23 @@ vertical_elements = {}
 for n in levels:
     # for each column in the level
     for i in levels[n]["key_cols"]:
-        v_element_node = levels[n]["nodes"][levels[n]["info_cols"][i]["node_j"]]
-        connected_elements = {}
-        vertical_elements[i] = dict(
-            connected_elements=connected_elements,
-            node_t=v_element_node,
-            level_id=n,
-        )
+
+        if i not in vertical_elements.keys():
+            v_element_node = levels[n]["nodes"][levels[n]["info_cols"][i]["node_j"]]
+            connected_elements = {}
+            vertical_elements[i] = dict(
+                connected_elements=connected_elements,
+                node_t=v_element_node,
+                level_id=n,
+            )
+        else:
+            v_element_node = levels[n]["nodes"][levels[n]["info_cols"][i]["node_j"]]
+            connected_elements = {}
+            vertical_elements[i+n] = dict(
+                connected_elements=connected_elements,
+                node_t=v_element_node,
+                level_id=n,
+            )
         # for each beam
         for j in levels[n]["key_beams"]:
 
@@ -331,7 +363,7 @@ for n in levels:
                     v_element_node["x_coord"],
                     v_element_node["y_coord"],
                 ]
-                # calcules the lenght and angle of the beam connected to the column
+                # calcules the length and angle of the beam connected to the column
                 beam_lenght = calculate_lenght(
                     beam_nodei_coordinates,
                     beam_nodej_coordinates,
@@ -344,16 +376,29 @@ for n in levels:
                 )
                 con = "this info will come from the prodes file"
                 # stores the information of the beam connected to the column
-                connected_elements[j] = extract_element(n, j, con, angle, "b*h, this info will come from a prodes file", "this will come from a prodes file", beam_lenght)
+                connected_elements[j] = extract_element(
+                    n,
+                    j,
+                    con,
+                    angle,
+                    "b*h, this info will come from a prodes file",
+                    "this will come from a prodes file",
+                    beam_lenght,
+                )
 
 
-building_columns = columns_assembler(vertical_elements)
+building_columns = columns_assembler(vertical_elements,0.001)
 
-#guardar building_columns en un archivo json
-with open('building_columns.json', 'w') as fp:
-    json.dump(building_columns, fp)
+# This is the way to verify if the columns are correct
+# Element
+item = "B12355"
 
-item='B12355'
-print(column_verify(item,building_columns,levels))
+#elements 
+print(column_verify(item, building_columns, levels))
 
-x = 2
+# guardar building_columns en un archivo json
+#with open("building_columns_midas.json", "w") as fp:
+#    json.dump(building_columns, fp)
+
+
+
